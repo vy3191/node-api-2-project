e89e5f67e3fe0534f1bb55989d10dbb1c872b0c9
vy3191 <venkyafhs@gmail.com>
2020-02-16 20:17:17 -0500

    Completed get post by id request

--- routers/posts-router.js
+++ routers/posts-router.js
@@ -8,8 +8,19 @@ router.get("/", async (req,res) => {
        if(posts.length === 0) res.status(404).json({msg:'There are no posts'});
        res.json(posts);
     } catch(err) {
-       res.status(500).json({msg:err});
+       res.status(500).json({ error: "The posts information could not be retrieved." });
     }
+})
+
+router.get("/:id", async (req,res) => {
+  try {
+     const id = req.params.id;
+     const post = await db.find(id);
+     if(!posts) res.status(404).json({message:`There are no posts with the ${id}`);
+     res.json(post);
+  } catch(err) {
+     res.status(500).json({ error: "The posts information could not be retrieved." });
+  }
 })
 
 
